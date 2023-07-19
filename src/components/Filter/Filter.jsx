0f27a1fa42bfa18407a6f5639f207821f3299c04
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-07-19 16:41:32 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,23 +1,24 @@
-import React from "react";
+import React from 'react';
 import propTypes from 'prop-types';
 import css from './Filter.module.css';
 
 const Filter = ({ filter, handleSearch }) => (
-    <div>
-        <label className={css.filterLabel}>Find contacts by Name </label>
-        <input 
-        className={css.filterName} 
-        type="text" 
-        name="filter" 
-        placeholder="Enter filter" 
-        value={filter} 
-        onChange={handleSearch}/>
-    </div>
+  <div>
+    <label className={css.filterLabel}>Find contacts by Name </label>
+    <input
+      className={css.filterName}
+      type="text"
+      name="filter"
+      placeholder="Enter filter"
+      value={filter}
+      onChange={handleSearch}
+    />
+  </div>
 );
 
 Filter.propTypes = {
-    filter: propTypes.string.isRequired,
-    handleSearch: propTypes.func.isRequired,
+  filter: propTypes.string.isRequired,
+  handleSearch: propTypes.func.isRequired,
 };
 
 export default Filter;
