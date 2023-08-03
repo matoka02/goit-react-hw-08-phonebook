2289a89d7fe5520d079d807129d166412d7c1f82
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-03 16:45:53 +0300

    Update contactSlice.js

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -1,24 +1,71 @@
 import { createSlice } from '@reduxjs/toolkit';
+
 import { contactsInitialState } from './initialState';
+import { addContactThunk, deleteContactThunk, getContactsThunk } from './operations';
+
+// const contactSlice = createSlice({
+//   name: 'phone',
+//   initialState: contactsInitialState,
+//   reducers: {
+//     addContact(state, { payload }) {
+//       // проверка на дубликаты
+//       let duplicate = state.contacts.find(contact => contact.name === payload.name);
+//       if (duplicate) {
+//         alert(`${payload.name} is already in contacts.`);
+//         return state;
+//       };
+
+//       state.contacts.push(payload);
+//     },
+//     deleteContact(state, { payload }) {
+//       state.contacts = state.contacts.filter(contact => contact.id !== payload);
+//     },
+//   },
+// });
 
 const contactSlice = createSlice({
   name: 'phone',
   initialState: contactsInitialState,
-  reducers: {
-    // addContact(state, action) {
-    addContact(state, { payload }) {
-      // проверка на дубликаты
-      let duplicate = state.contacts.find(contact => contact.name === payload.name);
-      if (duplicate) {
-        alert(`${payload.name} is already in contacts.`);
-        return state;
-      };
+  extraReducers: builder => {
+    builder
+      .addCase(getContactsThunk.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(getContactsThunk.fulfilled, (state, action) => {
+        state.items = action.payload;
+        state.error = null;
+        state.isLoading = false;
+      })
+      .addCase(getContactsThunk.rejected, (state, action) => {
+        state.error = action.payload;
+        state.isLoading = false;
+      })
 
-      state.contacts.push(payload);
-    },
-    deleteContact(state, { payload }) {
-      state.contacts = state.contacts.filter(contact => contact.id !== payload);
-    },
+      .addCase(deleteContactThunk.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(deleteContactThunk.fulfilled, (state, action) => {
+        state.items = state.items.filter(item => item.id !== action.payload);
+        state.error = null;
+        state.isLoading = false;
+      })
+      .addCase(deleteContactThunk.rejected, (state, action) => {
+        state.error = action.payload;
+        state.isLoading = false;
+      })
+
+      .addCase(addContactThunk.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(addContactThunk.fulfilled, (state, action) => {
+        state.items = [action.payload, ...state.items];
+        state.error = null;
+        state.isLoading = false;
+      })
+      .addCase(addContactThunk.rejected, (state, action) => {
+        state.error = action.payload;
+        state.isLoading = false;
+      });
   },
 });
 
