0c21ae13577bca2c94fd9170c3cb7d49432b320c
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-05 14:34:28 +0300

    create Layout

---
+++ src/components/Layot.jsx
@@ -0,0 +1,17 @@
+import { Suspense } from "react";
+import { Outlet } from "react-router-dom";
+
+import AppBar from "./AppBar/AppBar";
+
+const Layout = () => {
+  return (
+    <div style={{ maxWidth: 960, margin: '0 auto', padding: '0 16px' }}>
+      <AppBar />
+      <Suspense fallback={null}>
+        <Outlet />
+      </Suspense>
+    </div>
+  );
+};
+
+export default Layout;
