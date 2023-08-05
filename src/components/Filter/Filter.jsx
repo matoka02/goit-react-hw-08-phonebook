8a2810e55bee1d17c7c53855079fb718ba712cd8
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-05 14:13:58 +0300

    update Filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import css from './Filter.module.css';
 import { filterContacts } from 'redux/contacts/filterSlice';
 import { selectFilter } from 'redux/contacts/selectors';
-// import { filter } from 'store/filter/action-filter';
+import css from './Filter.module.css';
 
 const Filter = () => {
-  // const filter = useSelector((state)=>state.filter);
   const filter = useSelector(selectFilter);
   const dispatch = useDispatch();
 
   // слушатель на инпут ввода (для поиска)
   const handleSearch = ({target}) => {
-    // dispatch(filterContacts(target.value));
     dispatch(filterContacts(target.value));
   };
 
   return (
     <div>
-      <label className={css.filterLabel}>Find contacts by Name </label>
+      <label className={css.filterLabel}>Find contacts by name... </label>
       <input
         className={css.filterName}
         type="text"
