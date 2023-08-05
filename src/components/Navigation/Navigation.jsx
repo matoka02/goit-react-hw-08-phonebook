9efe57cb6d3a243b6e0d549b6c5a18f0e3eb6e90
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-05 13:57:51 +0300

    create Navigation

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,23 @@
+import { NavLink } from 'react-router-dom';
+
+import { useAuth } from 'hooks/useAuth';
+import css from './Navigation.module.css';
+
+const Navigation = () => {
+  const { isLoggedIn } = useAuth();
+
+  return (
+    <nav>
+      <NavLink className={css.link} to="/">
+        Home
+      </NavLink>
+      {isLoggedIn && (
+        <NavLink className={css.link} to="/contacts">
+          Contacts
+        </NavLink>
+      )}
+    </nav>
+  );
+};
+
+export default Navigation;
