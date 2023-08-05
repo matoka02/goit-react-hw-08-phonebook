758e5c69f05b9e11a8f2db5622e4a25ab6fedfdb
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-05 13:39:03 +0300

    Create useAuth.js

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,16 @@
+const { useSelector } = require("react-redux");
+const { selectIsLoggedIn, selectIsRefreshing, selectUser } = require("redux/auth/selectors");
+
+const useAuth = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isRefreshing = useSelector(selectIsRefreshing);
+  const user = useSelector(selectUser);
+
+  return {
+    isLoggedIn,
+    isRefreshing,
+    user,
+  };
+};
+
+export default useAuth;
