11c8e2adf0d3dcd313ce8f02b8e3859c77d31a56
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-05 14:37:42 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,35 +9,36 @@ import { getError, getIsLoading, selectContacts } from 'redux/contacts/selectors
 import Loader from './Loader/Loader';
 
 export const App = () => {
-  const dispatch = useDispatch();
-  const error = useSelector(getError);
-  const isLoading = useSelector(getIsLoading);
-  const users = useSelector(selectContacts);
-  console.log(users);
+  // const dispatch = useDispatch();
+  // const error = useSelector(getError);
+  // const isLoading = useSelector(getIsLoading);
+  // const users = useSelector(selectContacts);
+  // console.log(users);
 
-  useEffect(() => {
-    dispatch(getContactsThunk());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(getContactsThunk());
+  // }, [dispatch]);
 
   return (
-    <div
-      style={{
-        // height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 20,
-        color: '#010101',
-      }}
-    >
-      <h1>Phonebook</h1>
-      <ContactForm />
-      <h2> Contacts</h2>
-      <Filter />
-      {isLoading && !error && <Loader />}
-      <ContactList />
-    </div>
+    <></>
+    // <div
+    //   style={{
+    //     // height: '100vh',
+    //     display: 'flex',
+    //     flexDirection: 'column',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     fontSize: 20,
+    //     color: '#010101',
+    //   }}
+    // >
+    //   <h1>Phonebook</h1>
+    //   <ContactForm />
+    //   <h2> Contacts</h2>
+    //   <Filter />
+    //   {isLoading && !error && <Loader />}
+    //   <ContactList />
+    // </div>
   );
 };
 
