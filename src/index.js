b7864774e4922071a531935d66426feae8324ccc
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-05 15:33:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
+
 import { persistor } from 'redux/store';
 import { store } from 'redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
-
 import { App } from 'components/App';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
