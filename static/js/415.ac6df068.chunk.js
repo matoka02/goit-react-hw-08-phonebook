2937cb5cf3d53788e1ecfdb2f42ee4db5aa8488e
matoka02 <120492521+matoka02@users.noreply.github.com>
2023-08-05 12:09:27 +0000

    Deploying to gh-pages from @ matoka02/goit-react-hw-08-phonebook@a8681502c643c6216b566c63c28716c7e2fcf586 🚀

---
+++ static/js/415.ac6df068.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});var i=n(3329),c={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function r(){return(0,i.jsx)("div",{style:c.container,children:(0,i.jsxs)("h1",{style:c.title,children:["Phonebook welcome page"," "]})})}}}]);
+//# sourceMappingURL=415.ac6df068.chunk.js.map
