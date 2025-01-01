20deab786a0e88b480380cfc69eea52f452a3978
Anastasiia <120492521+matoka02@users.noreply.github.com>
2025-01-01 22:35:54 +0200

    add theme

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
 
 import { persistor } from 'store/store';
 import { store } from 'store/store';
@@ -13,13 +14,31 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      dark: '#85243b',
+      main: '#be3455',
+      light: '#cb5c77',
+    },
+    secondary: {
+      dark: '#ab003c',
+      main: '#f50057',
+      light: '#f73378',
+    },
+  },
+});
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename='goit-react-hw-08-phonebook' >
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor} >
-          <App />
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+          </ThemeProvider>
         </PersistGate>
       </Provider>
     </BrowserRouter>
